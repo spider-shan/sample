f07d7d36a64ef4b4a4513ad7d9307d9efb50bf52
Roshan <roshans20082004@gmail.com>
2025-05-26 10:54:27 +0000

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <>
+        <section className="flex flex-col itmes-centre justify-centre min-h-[80vh] px-4">
+            <div>Home</div>
+        </section>
+        </>  
+    );
+}
+export default Home;
